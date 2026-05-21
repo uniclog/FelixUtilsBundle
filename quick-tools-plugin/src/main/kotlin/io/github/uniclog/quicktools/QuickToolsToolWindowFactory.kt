eff8e0f96972cb68f577d94d540d3eda59253184
Denis V. <g.sstrav@gmail.com>
2026-05-21 14:35:41 +0300

    qtp: add refresh button

--- quick-tools-plugin/src/main/kotlin/io/github/uniclog/quicktools/QuickToolsToolWindowFactory.kt
+++ quick-tools-plugin/src/main/kotlin/io/github/uniclog/quicktools/QuickToolsToolWindowFactory.kt
@@ -3,9 +3,12 @@ package io.github.uniclog.quicktools
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.jcef.JBCefApp
 import com.intellij.ui.jcef.JBCefBrowser
+import java.awt.BorderLayout
+import javax.swing.JButton
 import javax.swing.JLabel
 import javax.swing.SwingConstants
 
@@ -18,8 +21,19 @@ class QuickToolsToolWindowFactory : ToolWindowFactory {
             return
         }
         
+        val panel = JBPanel<JBPanel<*>>(BorderLayout())
         val browser = JBCefBrowser("https://myip.ru")
-        val content = ContentFactory.getInstance().createContent(browser.component, "", false)
+        
+        // Toolbar for control
+        val toolbar = JBPanel<JBPanel<*>>()
+        val refreshButton = JButton("Refresh")
+        refreshButton.addActionListener { browser.loadURL("https://myip.ru") }
+        toolbar.add(refreshButton)
+        
+        panel.add(toolbar, BorderLayout.NORTH)
+        panel.add(browser.component, BorderLayout.CENTER)
+        
+        val content = ContentFactory.getInstance().createContent(panel, "", false)
         toolWindow.contentManager.addContent(content)
     }
 }
